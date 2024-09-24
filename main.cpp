10c3030a8e533a8ac8f99da85aa3c8d7140deed6
ville <ville.ailunka@student.oulu.fi>
2024-09-24 13:25:50 +0300

    testi 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include "src/calculator.h"
 
 int main() {
-    // Function calls to calculate
-    // Write your all possible input scenarios here
-    // Notice that calculator code written here works with integers
+        // additon
+    calculator(0, '+', 0);
+    calculator(1, '+', 1);
+    calculator(1.1, '+', 1.1);
+    calculator(-1, '+', -1);
+    calculator(10000000000, '+', 10000000000);    
+    // subtraction
+    calculator(0, '-', 0);
+    calculator(2.2, '-', 1.2);
+    calculator(-1, '-', -1);
+    calculator(10000000000000, '-',100000000);
+    // multiplication
+    calculator(2.2, '*', 2);
+    calculator(2, '*', 0);
+    calculator(-2, '*', 2);
+    calculator(-2, '*', -2);
+    calculator(1000000000, '*', 10000000000);
+    // dividing
+    calculator(4.2, '/', 2);
+    calculator(4, '/', 0);
+    calculator(4, '/', -2);
+    calculator(40000000, '/', 4000);	
+   //character testing
+   calculator(1, '&', 1);
     return 0;
 }
